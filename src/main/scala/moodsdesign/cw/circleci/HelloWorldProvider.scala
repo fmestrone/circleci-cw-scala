4daf0798b8e08ed6ca42a19ea356760362b1fa90
Federico Mestrone <federico@moodsdesign.com>
2018-01-19 18:11:49 +0000

    Same project with passing test

--- src/main/scala/moodsdesign/cw/circleci/HelloWorldProvider.scala
+++ src/main/scala/moodsdesign/cw/circleci/HelloWorldProvider.scala
@@ -1,5 +1,7 @@
 package moodsdesign.cw.circleci
 
 class HelloWorldProvider(val name: String = "world") {
+    if (name.equalsIgnoreCase("java"))
+        throw new IllegalArgumentException("We don't do Java anymore")
     def getHelloWorld = s"Hello $name!"
 }
--- src/test/scala/moodsdesign/cw/circleci/HelloWorldProviderSpec.scala
+++ src/test/scala/moodsdesign/cw/circleci/HelloWorldProviderSpec.scala
@@ -6,19 +6,19 @@ class HelloWorldProviderSpec extends FlatSpec with Matchers {
 
   "A HelloWorldProvider" should "greet the world if no name is given" in {
     val provider = new HelloWorldProvider()
-    provider.getHelloWorld should contain ("world")
+    provider.getHelloWorld should include ("world")
   }
 
   it should "greet the person specified" in {
     val name = "Fed"
     val provider = new HelloWorldProvider(name)
-    provider.getHelloWorld should contain (name)
+    provider.getHelloWorld should include (name)
   }
 
   it should "throw an exception if Java is mentioned" in {
     val name = "Java"
-    val provider = new HelloWorldProvider(name)
-    the [IllegalArgumentException] thrownBy provider.getHelloWorld should have message ("We don't do Java anymore")
+    the [IllegalArgumentException] thrownBy
+      new HelloWorldProvider(name) should have message ("We don't do Java anymore")
   }
 
 }
